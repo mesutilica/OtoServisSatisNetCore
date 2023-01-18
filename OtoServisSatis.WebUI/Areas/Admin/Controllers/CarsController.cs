406c8397076c4749114fa5c8a82ca7c3e00ed892
mesutilica <mesut.ilica@gmail.com>
2023-01-18 19:31:41 +0300

    admin

---
+++ OtoServisSatis.WebUI/Areas/Admin/Controllers/CarsController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OtoServisSatis.Entities;
+using OtoServisSatis.Service.Abstract;
+
+namespace OtoServisSatis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarsController : Controller
+    {
+        private readonly IService<Arac> _service;
+        private readonly IService<Marka> _serviceMarka;
+
+        public CarsController(IService<Arac> service, IService<Marka> serviceMarka)
+        {
+            _service = service;
+            _serviceMarka = serviceMarka;
+        }
+
+        // GET: CarsController
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: CarsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarsController/Create
+        public async Task<ActionResult> CreateAsync()
+        {
+            ViewBag.MarkaId = new SelectList(await _serviceMarka.GetAllAsync(), "Id", "Adi");
+            return View();
+        }
+
+        // POST: CarsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Arac arac)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.AddAsync(arac);
+                    await _service.SaveAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            ViewBag.MarkaId = new SelectList(await _serviceMarka.GetAllAsync(), "Id", "Adi");
+            return View(arac);
+        }
+
+        // GET: CarsController/Edit/5
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            ViewBag.MarkaId = new SelectList(await _serviceMarka.GetAllAsync(), "Id", "Adi");
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: CarsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Arac arac)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _service.Update(arac);
+                    await _service.SaveAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            ViewBag.MarkaId = new SelectList(await _serviceMarka.GetAllAsync(), "Id", "Adi");
+            return View(arac);
+        }
+
+        // GET: CarsController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: CarsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteAsync(int id, Arac arac)
+        {
+            try
+            {
+                _service.Delete(arac);
+                await _service.SaveAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OtoServisSatis.WebUI/Areas/Admin/Controllers/CustomersController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OtoServisSatis.Entities;
+using OtoServisSatis.Service.Abstract;
+
+namespace OtoServisSatis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CustomersController : Controller
+    {
+        private readonly IService<Musteri> _service;
+        private readonly IService<Arac> _serviceArac;
+
+        public CustomersController(IService<Musteri> service, IService<Arac> serviceArac)
+        {
+            _service = service;
+            _serviceArac = serviceArac;
+        }
+
+        // GET: CustomersController
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: CustomersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomersController/Create
+        public async Task<ActionResult> CreateAsync()
+        {
+            ViewBag.AracId = new SelectList(await _serviceArac.GetAllAsync(), "Id", "Modeli");
+            return View();
+        }
+
+        // POST: CustomersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Musteri musteri)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.AddAsync(musteri);
+                    await _service.SaveAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            ViewBag.AracId = new SelectList(await _serviceArac.GetAllAsync(), "Id", "Modeli");
+            return View(musteri);
+        }
+
+        // GET: CustomersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
