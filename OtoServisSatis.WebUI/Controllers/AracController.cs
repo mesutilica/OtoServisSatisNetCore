aebea03d01714e111d280da1b2298a72ad996954
Mesut ILICA <mesutilica@outlook.com>
2023-08-29 08:54:48 +0300

    Mesaj ve e mail bildirim sistemi eklendi

--- OtoServisSatis.WebUI/Controllers/AracController.cs
+++ OtoServisSatis.WebUI/Controllers/AracController.cs
@@ -2,6 +2,9 @@
 using OtoServisSatis.Entities;
 using OtoServisSatis.Service.Abstract;
 using OtoServisSatis.Service.Concrete;
+using OtoServisSatis.WebUI.Models;
+using OtoServisSatis.WebUI.Utils;
+using System.Security.Claims;
 
 namespace OtoServisSatis.WebUI.Controllers
 {
@@ -9,15 +12,46 @@ namespace OtoServisSatis.WebUI.Controllers
     {
         private readonly ICarService _serviceArac;
         private readonly IService<Musteri> _serviceMusteri;
-        public AracController(ICarService serviceArac, IService<Musteri> serviceMusteri)
+        private readonly IUserService _service;
+        public AracController(ICarService serviceArac, IService<Musteri> serviceMusteri, IUserService service)
         {
             _serviceArac = serviceArac;
             _serviceMusteri = serviceMusteri;
+            _service = service;
         }
 
-        public async Task<IActionResult> IndexAsync(int id)
+        public async Task<IActionResult> IndexAsync(int? id)
         {
-            var model = await _serviceArac.GetCustomCar(id);
+            if (id == null)
+                return BadRequest();
+
+            
+            var arac = await _serviceArac.GetCustomCar(id.Value);
+            if (arac == null)
+                return NotFound();
+            var model = new CarDetailViewModel();
+            model.Arac = arac;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var email = User.FindFirst(ClaimTypes.Email)?.Value;
+                var uguid = User.FindFirst(ClaimTypes.UserData)?.Value;
+                if (!string.IsNullOrEmpty(email) || !string.IsNullOrEmpty(uguid))
+                {
+                    var user = _service.Get(k => k.Email == email && k.UserGuid.ToString() == uguid);
+                    if (user != null)
+                    {
+                        model.Musteri = new Musteri
+                        {
+                            Adi = user.Adi,
+                            Soyadi = user.Soyadi,
+                            Email = user.Email,
+                            Telefon = user.Telefon                            
+                        };
+                    }
+                }
+            }
+
             return View(model);
         }
         [Route("tum-araclarimiz")]
@@ -40,10 +74,13 @@ namespace OtoServisSatis.WebUI.Controllers
                 {
                     await _serviceMusteri.AddAsync(musteri);
                     await _serviceMusteri.SaveAsync();
+                    // await MailHelper.SendMailAsync(musteri);
+                    TempData["Message"] = "<div class='alert alert-success'>Talebiniz Alınmıştır. Teşekkürler..</div>";
                     return Redirect("/Arac/Index/" + musteri.AracId);
                 }
                 catch
                 {
+                    TempData["Message"] = "<div class='alert alert-danger'>Bir Hata Oluştu!</div>";
                     ModelState.AddModelError("", "Hata Oluştu!");
                 }
             }
